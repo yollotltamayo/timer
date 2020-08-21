e6714a1a4301d132db66ef8cfd8017da9992084f
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2020-08-21 14:52:10 -0500

    Updates

---
+++ precache-manifest.e2c4798c3b34e4397b70e19c07fb32d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f0a3ff14d17dd48ad7e4be6b34b3b4",
+    "url": "/timer/index.html"
+  },
+  {
+    "revision": "6d425df64eb1f0c21b84",
+    "url": "/timer/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d28e84b8a6bec54161d9",
+    "url": "/timer/static/css/main.64a804bd.chunk.css"
+  },
+  {
+    "revision": "6d425df64eb1f0c21b84",
+    "url": "/timer/static/js/2.871e4482.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/timer/static/js/2.871e4482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d28e84b8a6bec54161d9",
+    "url": "/timer/static/js/main.0ab10013.chunk.js"
+  },
+  {
+    "revision": "6ca0cd32a48719abed3d",
+    "url": "/timer/static/js/runtime-main.7b2887db.js"
+  }
+]);
